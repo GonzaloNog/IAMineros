fd71d00b0ef2dda3bf65c61a68ff2f9059362147
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2020-11-16 10:53:46 -0300

    Grid

---
+++ IA Mineros/Assets/Script/Testing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+    private void Start()
+    {
+        grid = new Grid(6, 3, 10f, this.transform.position);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
